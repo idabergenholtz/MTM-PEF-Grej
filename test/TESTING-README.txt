a2b6f48aa303918d8248d09e75b8ff9f31a8d94a
Alex Renfors <0118real@jonkoping.se>
2022-09-29 12:26:45 +0200

    Addition of some instructions and configuration necessary to run JEST

---
+++ test/TESTING-README.txt
@@ -0,0 +1,7 @@
+1. Säg till att du har NPM installerat
+
+2. Installera JEST biblioteket genom
+    npm install --save-dev JEST
+
+3. Du kan nu köra testerna genom
+    npm test
---
+++ test/test.test.js
@@ -0,0 +1,9 @@
+const incr = require('../incr.js');
+
+test('tests an increment', () => {
+    expect(1 + 1).toBe(2);
+});
+
+// test('tests a faulty increment', () => {
+//     expect(1 + 1).toBe(3);
+// });
