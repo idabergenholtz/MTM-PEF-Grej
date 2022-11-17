5b4c2640ba04b6202de5f1ac69a1bd1284d9420c
Daniel Wilsson <wilsson.danne@gmail.com>
2022-11-17 15:56:17 +0100

    Refaktorisering
    ändrade vad som returneras från funktioner

--- src/translator.js
+++ src/translator.js
@@ -14,89 +14,90 @@ export function translateToSwedish(braille) {
         }
         //Check for lowercase letters
         temp = findInTable(currentChar, alphabetTable)
-        if(temp[0]) {
-            string+=alphabetTable[0][temp[1]]
+
+        if(temp > -1) {
+            string += alphabetTable[0][temp]
             continue
         }
-        //Check for punctuation, parentheses, currency signs, etc
+        //skiljetecken och specialtecken som i punktskrift består av två tecken
         if(i + 1 < braille.length) {
             let nextChar = braille.charAt(i+1)
             temp = findInTable(currentChar + nextChar, doubleCharTable)
 
-            if(temp[0]) {
-                string+=doubleCharTable[0][temp[1]]
+            if(temp > -1) {
+                string += doubleCharTable[0][temp]
                 i++
                 continue
             }
         }
+        //skiljetecken och specialtecken som i punktskrift består av ett tecken
         temp = findInTable(currentChar, singleCharTable)
-        if(temp[0]) {
-            string+=singleCharTable[0][temp[1]]
+
+        if(temp > -1) {
+            string+=singleCharTable[0][temp]
             continue
         }
 
-        //Check for an uppercase sign
+        //Check for uppercase characters
         temp = uppercase(braille, i)
-        if(temp[0]) {
-            string += temp[1]
-            i+=temp[2]
+        if(temp.length > 0) {
+            string += temp
+            i+= temp.length
             continue
         }
-
-        //Check for a number sign
+   
+        //Check for number signs
         temp = number(braille, i)
-        if(temp[0]) {
-            string += temp[1]
-            i+=temp[2]
+        if(temp.length > 0) {
+            string += temp
+            i+=temp.length
             continue
         }
 
-        string += currentChar //Eller currentChar
+        string += currentChar //No suitable replacement found, add the braille char to the string
     }
     return string
 }
 
 function uppercase(braille, index) {
-    if(braille.charAt(index) !== uppercaseSign) return [false] //Det ska inte vara en versal
+    if(braille.charAt(index) !== uppercaseSign) return "" //Det ska inte vara en versal
     let count = 1
     let string = ""
 
     //Endast första bokstaven i ordet ska vara en versal
     if(braille.charAt(index+1) !== uppercaseSign) {
         let temp = findInTable(braille.charAt(index+1), alphabetTable)
-        if(temp[0]) string += alphabetTable[2][temp[1]]
-        
-        return [true,string,count]
+        if(temp > -1) string += alphabetTable[2][temp]
+        //return [true,string,count]
+        return string
     }
 
     //Hela ordet, fram till nästa "icke-bokstav" ska vara versaler
     for(let i = index+2; i < braille.length; i++) {
         let temp = findInTable(braille.charAt(i), alphabetTable)    
-        if(!temp[0]) break
+        if(temp <= -1) break
         count++
-        string += alphabetTable[2][temp[1]]
+        string += alphabetTable[2][temp]
     }
-    return [true, string, count]
+    return string
 }
 
 function number(braille, index) {
-    //Undersök sida 49 i pdf, ex: 100.000.000
-    if(braille.charAt(index) !== numberSign) return [false] 
+    if(braille.charAt(index) !== numberSign) return "" 
     let count = 0
     let string = ""
 
     for(let i = index+1; i < braille.length; i++) {
         let temp = findInTable(braille.charAt(i), numbersTable)
-        if(!temp[0]) break
+        if(temp <= -1) break
         count++
-        string += numbersTable[0][temp[1]]
+        string += numbersTable[0][temp]
     }
-    
-    return [true, string, count]
+    return string
 }
 
 function findInTable(currentChars, table) {
     let index = table[1].indexOf(currentChars)
-    if(index == -1) return [false]
-    return [true, index]
+
+    return index;
 }
