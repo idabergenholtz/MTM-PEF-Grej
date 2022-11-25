550e2d9b995a659e8f8921770965f402e1d1dbda
Buffy <johan@bergenholtz.se>
2022-11-25 14:43:02 +0100

    Small correction

--- src/index.js
+++ src/index.js
@@ -179,7 +179,7 @@ function changeCurrentPage() {
 
 function pageChangeFinished() {
     if (htmlPageInput.value === ""){
-        displayCurrentPage(false, false, true);
+        displayCurrentPage(false);
     }
     else{
         displayCurrentPage();
